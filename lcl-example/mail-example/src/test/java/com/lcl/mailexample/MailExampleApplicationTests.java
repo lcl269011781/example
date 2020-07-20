a1fef7dbd1608b624541c5258f77de5b9b564b0b
lcl <269011781@qq.com>
2020-07-20 10:31:05 +0800

    未完成shiro

---
+++ lcl-example/mail-example/src/test/java/com/lcl/mailexample/MailExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcl.mailexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ lcl-example/shiro-example/src/test/java/com/lcl/shiro/ShiroExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcl.shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
