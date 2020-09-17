87275a118aa86b2fd60e97cecdd8d52e5aafe2a0
269011781 <lcl269011781@aliyun.com>
2020-09-17 14:27:11 +0800

    修改线程池配置方式

--- lcl-example/easyexcel-example/src/main/java/com/lcl/easyexcel/executor/ThreadPoolManager.java
+++ lcl-example/easyexcel-example/src/main/java/com/lcl/easyexcel/executor/ThreadPoolManager.java
@@ -1,7 +1,6 @@
 package com.lcl.easyexcel.executor;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,22 +15,48 @@ import java.util.concurrent.*;
  * 本人学识渊博、经验丰富，代码风骚、效率恐怖、无所不精、无所不通
  **/
 @Configuration
+@Slf4j
 public class ThreadPoolManager {
-    @Autowired
-    private EasyExcelWriterThreadFactory easyExcelWriterThreadFactory;
-    @Autowired
-    private PoolRejectedExecutionHandler poolRejectedExecutionHandler;
 
     @Bean
     public ThreadPoolExecutor easyrExcelThreadPool(@Value("${threadpool.easyexcel.corePoolSize:5}") int corePoolSize,
                                                    @Value("${threadpool.easyexcel.maximumPoolSize:20}") int maximumPoolSize,
+                                                   @Value("${threadpool.easyexcel.keepAliveTime:30}") long keepAliveTime,
                                                    @Value("${threadpool.easyexcel.queueSize:100}") int queueSize
+
     ) {
+        log.info("easyexcel-pool线程池初始化,corePoolSize:【{}】,maximumPoolSize:【{}】,keepAliveTime:【{}】," +
+                "queueSize:【{}】", corePoolSize, maximumPoolSize, keepAliveTime, queueSize);
         return new ThreadPoolExecutor(corePoolSize,
                 maximumPoolSize,
-                30,
+                keepAliveTime,
                 TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(queueSize),
-                easyExcelWriterThreadFactory, poolRejectedExecutionHandler);
+                r -> new Thread(r, "easyexcel-pool"), new PoolRejectedExecutionHandler());
     }
+
+    /**
+     * 自定义拒绝策略
+     */
+    public static class PoolRejectedExecutionHandler implements RejectedExecutionHandler {
+        public PoolRejectedExecutionHandler() {
+        }
+
+        @Override
+        public void rejectedExecution(Runnable runnable, ThreadPoolExecutor threadPoolExecutor) {
+            log.info("{},总任务数:【{}】,已完成任务数:【{}】,线程活跃数:【{}】,排队数:【{}】",
+                    threadPoolExecutor,
+                    threadPoolExecutor.getTaskCount() + 1,
+                    threadPoolExecutor.getCompletedTaskCount(),
+                    threadPoolExecutor.getActiveCount() + 1,
+                    threadPoolExecutor.getQueue().size());
+            //如果线程池满了使用调用者线程执行任务
+            if (!threadPoolExecutor.isShutdown()) {
+                runnable.run();
+            }
+
+        }
+    }
+
+
 }
